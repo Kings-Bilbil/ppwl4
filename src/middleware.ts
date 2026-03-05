0b0d1b6acdb471a4d7ea2c1205261cd064d5de97
Kings-Bilbil <nabilnur556@gmail.com>
2026-03-05 22:41:46 +0700

    Menyelesaikan Praktikum 6: afterHandle

--- src/middleware.ts
+++ src/middleware.ts
@@ -37,6 +37,21 @@ const app = new Elysia()
       },
     }
   )
+  .get(
+    "/product",
+    () => {
+      return { id: 1, name: "Laptop" };
+    },
+    {
+      afterHandle({ response }) {
+        return {
+          success: true,
+          Message: "data tersedia",
+          data: response,
+        };
+      },
+    }
+  )
   .listen(3000);
 
 console.log(`Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
